b04808ae135f5d76f800127b47b8b8e83586cbce
ThiagoMASS <thiago.limamassa2017@gmail.com>
2024-08-29 11:31:18 -0400

    testes openCV

---
+++ testes.opencv/face.py
@@ -0,0 +1,14 @@
+import cv2
+camera = cv2.VideoCapture(0)
+classificador = cv2.CascadeClassifier(r'inovatech/haarcascade_frontalface_default.xml')
+
+while True :
+    check,img= camera.read()
+    imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    objetos = classificador.detectMultiScale(imgGray,minSize=(100,100))#vai detectar onde está o rosto em cordenadas 
+
+    for x,y,l,a in objetos:#pegamos as variaveis x y l a para as cordenadas e iteramos com o for para objetos que é onde retorna as cordenadas
+        cv2.rectangle(img,(x,y),(x+l,y+a),(255,0,0),2)
+
+    cv2.imshow('imagem',img)
+    cv2.waitKey(1)
